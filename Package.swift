0d7497f9b2e82c97faf4c6cea5dc20f52f21670f
Måns Bernhardt <mans.bernhardt@gmail.com>
2024-09-28 12:07:59 +0200

    Require 1.3.7 of swift-dependencies to compile with Swift 6.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.3.7"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.3.9"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.0"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "509.0.0"),
